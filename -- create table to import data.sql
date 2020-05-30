90745dfa0aa015731f8cfe5df476e3df17fd0417
Emmarose McCoig <emmamccoig@gmail.com>
2020-05-30 14:00:28 -0500

    final commit

---
+++ -- create table to import data.sql
@@ -0,0 +1,22 @@
+-- create table to import data
+
+CREATE TABLE candy (
+id INT PRIMARY KEY,
+ndb_no text,
+competitorname TEXT,
+water_g INT,
+energy_kcal INT,
+protein_g INT,
+sugar_g INT,
+chocolate INT,
+fruity INT,
+caramel INT,
+peanutyalmondy INT,
+crispedricewafer INT,
+hard INT,
+bar INT,
+pluribus INT,
+sugarpercent INT,
+pricepercent INT, 
+winpercent INT
+);
---
+++ QuickDBD-export (13).sql
@@ -0,0 +1,29 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/Zy21cQ
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "candy" (
+    "id" int,   NOT NULL,
+    "ndb_no" text,   NOT NULL,
+    "competitorname" TEXT,   NOT NULL,
+    "water_g" INT,   NOT NULL,
+    "energy_kcal" INT,   NOT NULL,
+    "protein_g" INT,   NOT NULL,
+    "sugar_g" INT,   NOT NULL,
+    "chocolate" INT,   NOT NULL,
+    "fruity" INT,   NOT NULL,
+    "caramel" INT,   NOT NULL,
+    "peanutyalmondy" INT,   NOT NULL,
+    "crispedricewafer" INT,   NOT NULL,
+    "hard" INT,   NOT NULL,
+    "bar" INT,   NOT NULL,
+    "pluribus" INT,   NOT NULL,
+    "sugarpercent" INT,   NOT NULL,
+    "pricepercent" INT,   NOT NULL,
+    "winpercent" INT   NOT NULL,
+    CONSTRAINT "pk_candy" PRIMARY KEY (
+        "id"
+     )
+);
+
